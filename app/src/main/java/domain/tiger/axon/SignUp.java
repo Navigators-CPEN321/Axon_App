e70468b86b308cff9e31b4876fd0deb5adb37f26
Brian Trieu <brian.trieu7@gmail.com>
2018-10-20 23:59:09 -0700

    Working version of sign-up, login, and preferences

--- app/src/main/java/domain/tiger/axon/SignUp.java
+++ app/src/main/java/domain/tiger/axon/SignUp.java
@@ -4,10 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -25,41 +27,86 @@ public class SignUp extends AppCompatActivity {
 
     private Button submit ;
 
+    private FirebaseFirestore db;
 
 
-    String username, email, password, address;
-    EditText usernameInput, emailInput, passwordInput, addressInput;
-
-
-    //setting up firebase reference
-    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/inspiration");
-
-    private DocumentReference userDocRef = FirebaseFirestore.getInstance().document("users/user4");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         submit = findViewById(R.id.doneButton);
-        usernameInput = findViewById(R.id.usernameInput);
-        emailInput = findViewById(R.id.emailInput);
-        passwordInput = findViewById(R.id.passwordInput);
-        addressInput = findViewById(R.id.addressInput);
+
+        db = FirebaseFirestore.getInstance();
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText emailInput = (EditText) findViewById(R.id.emailInput);
+                EditText passwordInput = (EditText) findViewById(R.id.passwordInput);
+                EditText addressInput = (EditText) findViewById(R.id.addressInput);
 
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                String address = addressInput.getText().toString();
 
+                Map<String, Object> userInfo = new HashMap<>();
 
-                backHome();
+                /*if (email.isEmpty()){
+                    emailInput.setError("Email is required.");
+                    emailInput.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    emailInput.setError("Invalid email.");
+                    emailInput.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()){
+                    passwordInput.setError("Password is required.");
+                    passwordInput.requestFocus();
+                    return;
+                }
+
+                if (password.length()<6){
+                    passwordInput.setError("Password needs to be longer than 6 characters");
+                    passwordInput.requestFocus();
+                    return;
+                }
+
+                if (address.isEmpty()){
+                    addressInput.setError("Address is required.");
+                    addressInput.requestFocus();
+                    return;
+                }*/
+
+                userInfo.put("email", email);
+                userInfo.put("password", password);
+                userInfo.put("location", address);
+
+                db.collection("users").add(userInfo).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(SignUp.this, "Congrats! You created an account. Start by creating a group or joining one!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(SignUp.this, group.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUp.this, "Sorry unable to create an account.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                //backHome();
             }
         });
 
     }
 
 
-    public void backHome(){
+    /*public void backHome(){
 
 
         username = usernameInput.getText().toString();
@@ -84,6 +131,6 @@ public class SignUp extends AppCompatActivity {
 
         //Intent i = new Intent(this, MainActivity.class);
         //startActivity(i);
-    }
+    }*/
 
 }
--- app/src/main/java/domain/tiger/axon/group_create.java
+++ app/src/main/java/domain/tiger/axon/group_create.java
@@ -1,13 +1,31 @@
 package domain.tiger.axon;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.opencensus.tags.Tag;
 
 public class group_create extends AppCompatActivity {
+
+    private Button groupCreateSubmit;
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +40,41 @@ public class group_create extends AppCompatActivity {
 
         getWindow().setLayout((int)(width*0.75), (int)(height * 0.75));
 
-        create_group();
-    }
+        /*
+        Creates a group using a name the user enters
+            -if successful -> go to that group's page
+            -if not successful -> notify the user
+         */
+        groupCreateSubmit = findViewById(R.id.btnGroupCreateSubmit);
 
-    public void create_group(){
-        Button btnGroupCreateSubmit = (Button) findViewById(R.id.btnGroupCreateSubmit);
-        btnGroupCreateSubmit.setOnClickListener(new View.OnClickListener() {
+        db = FirebaseFirestore.getInstance();
+
+        groupCreateSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(group_create.this, group_view.class));
+                EditText groupNameView = (EditText) findViewById(R.id.groupName);
+                String groupName = groupNameView.getText().toString();
+                if (groupName.isEmpty()) {
+                    return;
+                }
+
+                Map<String, String> groupNameMap = new HashMap<>();
+                groupNameMap.put("group_name", groupName);
+
+                db.collection("groups").add(groupNameMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(group_create.this, "Congrats! You created a group. Now invite some friends!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(group_create.this, group_view.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(group_create.this, "Group creation failed.", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
+
 }
