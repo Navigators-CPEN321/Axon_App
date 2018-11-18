3afb5cc975fb47faf51edcb157a35237806d60bc
Tiger Zhong <tiger.zhong.turbo@gmail.com>
2018-11-17 18:21:01 -0800

    updated to create group test

--- app/src/androidTest/java/domain/tiger/axon/CreateGroup2Test.java
+++ app/src/androidTest/java/domain/tiger/axon/CreateGroup2Test.java
@@ -1,37 +1,31 @@
 package domain.tiger.axon;
 
 
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import static androidx.test.InstrumentationRegistry.getInstrumentation;
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.*;
-import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
-
-import domain.tiger.axon.R;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -42,97 +36,71 @@ public class CreateGroup2Test {
 
     @Test
     public void createGroup2Test() {
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-try {
- Thread.sleep(7000);
- } catch (InterruptedException e) {
- e.printStackTrace();
- }
-        
+
+
         ViewInteraction appCompatEditText = onView(
-allOf(withId(R.id.editTextEmail),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-1),
-isDisplayed()));
+                allOf(withId(R.id.editTextEmail),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
         appCompatEditText.perform(replaceText("bob@ubc.ca"), closeSoftKeyboard());
-        
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-try {
- Thread.sleep(7000);
- } catch (InterruptedException e) {
- e.printStackTrace();
- }
-        
-        ViewInteraction appCompatEditText2 = onView(
-allOf(withId(R.id.editTextPassword),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-2),
-isDisplayed()));
-        appCompatEditText2.perform(replaceText("12345"), closeSoftKeyboard());
-        
-        pressBack();
-        
-        ViewInteraction appCompatEditText3 = onView(
-allOf(withId(R.id.editTextPassword), withText("12345"),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-2),
-isDisplayed()));
-        appCompatEditText3.perform(replaceText("123456"));
-        
-        ViewInteraction appCompatEditText4 = onView(
-allOf(withId(R.id.editTextPassword), withText("123456"),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-2),
-isDisplayed()));
-        appCompatEditText4.perform(closeSoftKeyboard());
-        
-        ViewInteraction appCompatButton = onView(
-allOf(withId(R.id.btnLogin), withText("Log in"),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-3),
-isDisplayed()));
-        appCompatButton.perform(click());
-        
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-try {
- Thread.sleep(7000);
- } catch (InterruptedException e) {
- e.printStackTrace();
- }
-        
-        ViewInteraction appCompatButton2 = onView(
-allOf(withId(R.id.btnGroupCreate), withText("CREATE A GROUP"),
-childAtPosition(
-allOf(withId(R.id.container),
-childAtPosition(
-withId(android.R.id.content),
-0)),
-1),
-isDisplayed()));
-        appCompatButton2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editTextPassword),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("123456"), closeSoftKeyboard());
+
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btnLogin), withText("Log in"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnGroupCreate), withText("CREATE A GROUP"),
+                        childAtPosition(
+                                allOf(withId(R.id.container),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+
+    }
+
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
@@ -147,8 +115,8 @@ isDisplayed()));
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup)parent).getChildAt(position));
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
-    }
+}
