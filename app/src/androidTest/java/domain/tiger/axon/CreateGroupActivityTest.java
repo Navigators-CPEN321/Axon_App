ed4108cd9063f08589660d44a243dacbbc9b6f31
Tiger Zhong <tiger.zhong.turbo@gmail.com>
2018-11-17 17:42:08 -0800

    added create group UI tests

---
+++ app/src/androidTest/java/domain/tiger/axon/CreateGroupActivityTest.java
@@ -0,0 +1,185 @@
+package domain.tiger.axon;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import domain.tiger.axon.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CreateGroupActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void createGroupActivityTest() {
+        ViewInteraction appCompatEditText = onView(
+allOf(withId(R.id.editTextEmail),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+1),
+isDisplayed()));
+        appCompatEditText.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(7000);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction appCompatEditText2 = onView(
+allOf(withId(R.id.editTextEmail),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+1),
+isDisplayed()));
+        appCompatEditText2.perform(replaceText("bob@ubc.ca"), closeSoftKeyboard());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(5000);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction appCompatEditText3 = onView(
+allOf(withId(R.id.editTextPassword),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+2),
+isDisplayed()));
+        appCompatEditText3.perform(replaceText("123456"), closeSoftKeyboard());
+        
+        pressBack();
+        
+        ViewInteraction appCompatButton = onView(
+allOf(withId(R.id.btnLogin), withText("Log in"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+3),
+isDisplayed()));
+        appCompatButton.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(7000);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction appCompatButton2 = onView(
+allOf(withId(R.id.btnGroupCreate), withText("CREATE A GROUP"),
+childAtPosition(
+allOf(withId(R.id.container),
+childAtPosition(
+withId(android.R.id.content),
+0)),
+1),
+isDisplayed()));
+        appCompatButton2.perform(click());
+        
+         // Added a sleep statement to match the app's execution delay.
+ // The recommended way to handle such scenarios is to use Espresso idling resources:
+  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+try {
+ Thread.sleep(7000);
+ } catch (InterruptedException e) {
+ e.printStackTrace();
+ }
+        
+        ViewInteraction appCompatEditText4 = onView(
+allOf(withId(R.id.etGroupName),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+0),
+isDisplayed()));
+        appCompatEditText4.perform(click());
+        
+        ViewInteraction appCompatEditText5 = onView(
+allOf(withId(R.id.etGroupName),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+0),
+isDisplayed()));
+        appCompatEditText5.perform(replaceText("a"), closeSoftKeyboard());
+        
+        pressBack();
+        
+        ViewInteraction appCompatButton3 = onView(
+allOf(withId(R.id.btnGroupCreateSubmit), withText("Submit"),
+childAtPosition(
+childAtPosition(
+withId(android.R.id.content),
+0),
+1),
+isDisplayed()));
+        appCompatButton3.perform(click());
+        }
+    
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup)parent).getChildAt(position));
+            }
+        };
+    }
+    }
