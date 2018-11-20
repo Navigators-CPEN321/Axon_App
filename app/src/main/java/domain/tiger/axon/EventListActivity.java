1ca6b00c66ceed318405db6a7554f84b4989704f
Brian Trieu <brian.trieu7@gmail.com>
2018-11-19 19:53:06 -0800

    Events catalog page added

---
+++ app/src/main/java/domain/tiger/axon/EventListActivity.java
@@ -0,0 +1,34 @@
+package domain.tiger.axon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class EventListActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseUser user = auth.getCurrentUser();
+    private String category;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+
+        db.collection("users").document(user.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                category = documentSnapshot.get("mostRecentEventsList").toString();
+                Toast.makeText(EventListActivity.this, category, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
