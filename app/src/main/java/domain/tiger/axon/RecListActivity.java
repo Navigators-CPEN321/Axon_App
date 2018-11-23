1a41ad6e98c03847af2b3e3c0f70f72056f80e48
Brian Trieu <brian.trieu7@gmail.com>
2018-11-22 18:11:01 -0800

    Changed RecListActivity to display recommended activities

--- app/src/main/java/domain/tiger/axon/RecListActivity.java
+++ app/src/main/java/domain/tiger/axon/RecListActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /*
@@ -19,56 +31,56 @@ import com.google.firebase.firestore.FirebaseFirestore;
  */
 public class RecListActivity extends AppCompatActivity {
 
-    private TextView event0text, event1text, event2text, event3text, event4text;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseUser user = auth.getCurrentUser();
+    private ListView lvRecList;
+    private ArrayList<String> selEventsList = new ArrayList<>();
+    private String currentGroup;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommended_activities_list);
 
+        //Connect listview
+        lvRecList = (ListView) findViewById(R.id.lvRecList);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, selEventsList);
+        lvRecList.setAdapter(adapter);
 
-
-        //Connect TextViews
-
-        event0text = (TextView) findViewById(R.id.event1);
-        event1text = (TextView) findViewById(R.id.event2);
-        event2text = (TextView) findViewById(R.id.event3);
-        event3text = (TextView) findViewById(R.id.event4);
-        event4text = (TextView) findViewById(R.id.event5);
-
-
-        for (int i = 0; i < 5; i++){
-            updateRecActivitiesList(i);
-        }
-
-    }
-
-    /*
-    Creates the list by taking eventnum and finding the associated recommended event.
-     */
-    private void updateRecActivitiesList(final int eventnum){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference ref;
-        ref = db.collection("events").document("event" + eventnum);
-        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        db.collection("users").document(user.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()){
-                    DocumentSnapshot doc = task.getResult();
-                    switch (eventnum){
-                        case 0: event0text.setText(doc.get("name").toString());
-                            break;
-                        case 1: event1text.setText(doc.get("name").toString());
-                            break;
-                        case 2: event2text.setText(doc.get("name").toString());
-                            break;
-                        case 3: event3text.setText(doc.get("name").toString());
-                            break;
-                        case 4: event4text.setText(doc.get("name").toString());
-                            break;
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                currentGroup = documentSnapshot.get("currentGroup").toString();
+                db.collection("groups").document(currentGroup).collection("sel_events").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> qsList = queryDocumentSnapshots.getDocuments();
+                        if (qsList.size() != 0){
+                            selEventsList.clear();
+                            for (int i = 0; i < qsList.size(); i++){
+                                selEventsList.add(qsList.get(i).get("name").toString());
+                            }
+                            Collections.sort(selEventsList, String.CASE_INSENSITIVE_ORDER);
+                            adapter.notifyDataSetChanged();
+                        } else{
+                            Toast.makeText(RecListActivity.this,
+                                    "No events found",
+                                    Toast.LENGTH_LONG).show();
+                        }
                     }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RecListActivity.this,
+                                "No events found",
+                                Toast.LENGTH_LONG).show();
                     }
-                }
-            });
+                });
+            }
+        });
     }
+
+
 }
